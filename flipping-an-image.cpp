05a1f6ce667266cfe8fbd7ed1297795be208f48c
Nishita Kadian <kadiannishita@gmail.com>
2020-12-25 05:30:00 +0530

    Adding solution to FLIPPING AN IMAGE

---
+++ flipping-an-image.cpp
@@ -0,0 +1,28 @@
+//FLIPPING AN IMAGE -> 12/25/2020 13:20
+//https://leetcode.com/problems/flipping-an-image/submissions/
+class Solution {
+public:
+    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& A) {
+        
+        for(int i=0;i<A.size();i++)
+        {
+            reverse(A[i].begin(),A[i].end());
+            
+            for(int j=0;j<A[i].size();j++)
+            {
+                if(A[i][j]==0)
+                {
+                    A[i][j]=1;
+                }
+                
+                else
+                {
+                    A[i][j]=0;
+                }
+                
+            }
+        }
+        
+        return A;
+    }
+};
