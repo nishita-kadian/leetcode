ef735919e1bcedac4367ff60586c049380044344
Nishita Kadian <kadiannishita@gmail.com>
2022-01-25 05:30:00 +0530

    Adding solution to NEXT PERMUTATION

---
+++ next-permutation.cpp
@@ -0,0 +1,9 @@
+//NEXT PERMUTATION -> 01/25/2022 22:56
+//https://leetcode.com/problems/next-permutation/submissions/
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        next_permutation(nums.begin(),nums.end());
+        return;
+    }
+};
