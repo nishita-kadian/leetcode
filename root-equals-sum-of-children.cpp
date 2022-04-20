c4fe66d88bbf249f08abf76be0fc0a578895307f
Nishita Kadian <kadiannishita@gmail.com>
2022-04-20 05:30:00 +0530

    Adding solution to ROOT EQUALS SUM OF CHILDREN

---
+++ root-equals-sum-of-children.cpp
@@ -0,0 +1,22 @@
+//ROOT EQUALS SUM OF CHILDREN -> 04/20/2022 16:48
+//https://leetcode.com/problems/root-equals-sum-of-children/submissions/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool checkTree(TreeNode* root) {
+        if(root->left->val + root->right->val == root->val)
+            return true;
+        return false;
+        
+    }
+};
