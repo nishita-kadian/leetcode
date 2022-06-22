de01c643983013658af807cf762b5a9fa220bbab
Nishita Kadian <kadiannishita@gmail.com>
2022-06-22 05:30:00 +0530

    Adding solution to KTH LARGEST ELEMENT IN AN ARRAY

---
+++ kth-largest-element-in-an-array.cpp
@@ -0,0 +1,18 @@
+//KTH LARGEST ELEMENT IN AN ARRAY -> 06/22/2022 09:24
+//https://leetcode.com/problems/kth-largest-element-in-an-array/submissions/
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int> pq;
+        for(int i = 0 ; i < k ; i++){
+            pq.push(-(nums[i]));
+        }
+        for(int i = k ; i < nums.size() ; i++){
+            if(-pq.top()<nums[i]){
+                pq.pop();
+                pq.push(-nums[i]);
+            }
+        }
+        return -(pq.top());
+    }
+};
